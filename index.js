0937249670977e22856641947515a5a3daf61136
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-01-11 17:30:09 +0600

    Today section has been done

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
-// state -- count : 0
-// action -- increment, decrement, reset
-// reducer
-// store
+// products constants
+const GET_PRODUCTS = "GET_PRODUCTS";
 
-const { createStore } = require("redux");
-
-// CONSTANTS
-const ADD_USER = "ADD_USER";
-
-// State
-const initialState = {
-  users: ["Shah Arefin Ahmed"],
-  count: 1,
+// state
+const initialProductState = {
+  products: ["sugar", "salt "],
+  numberofProducts: 2,
 };
 
-// action -- Object -- type, payload
-const addUser = (user) => {
-  return {
-    type: ADD_USER,
-    payload: user,
-  };
-};
+// action
+const getProducts = () => {};
 
-// CREATING REDUCER - Reducer does logic handle
-const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_USER:
-      return {
-        users: [...state.users, action.payload],
-        count: state.count + 1,
-      };
-
-    default:
-      state;
-  }
-};
-
-// store
-const store = createStore(userReducer);
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch(addUser("Farok Hossain"));
-store.dispatch(addUser("Omor Rabby"));
+// cartReducer
